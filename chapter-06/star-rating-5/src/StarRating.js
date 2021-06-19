f3e9b2d5af49d34a6597966f94962c3586cf431d
K Tact <ktact.dev@gmail.com>
2021-06-19 11:29:23 +0900

    StarRagtingコンポーネントにスタイルを指定できるようインタフェースを変更

--- chapter-06/star-rating-5/src/StarRating.js
+++ chapter-06/star-rating-5/src/StarRating.js
@@ -7,10 +7,10 @@ const Star = ({ selected = false, onSelect = f => f }) => (
   <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
 
-export default function StarRating({ totalStars = 5 }) {
+export default function StarRating({ style = {}, totalStars = 5 }) {
   const [selectedStars, setSelectedStars] = useState(0);
   return (
-    <>
+    <div style={{ padding: "5px", ...style }}>
       {createArray(totalStars).map((n, i) => (
         <Star
           key={i}
@@ -21,6 +21,6 @@ export default function StarRating({ totalStars = 5 }) {
       <p>
         {selectedStars} of {totalStars} stars
       </p>
-    </>
+    </div>
   );
 }
