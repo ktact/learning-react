b4c080e5c14be8a7a2002c2d95f38e9ea802df80
K Tact <ktact.dev@gmail.com>
2021-06-19 13:56:09 +0900

    レーティング機能実装

--- chapter-06/color-organizer/src/ColorList.js
+++ chapter-06/color-organizer/src/ColorList.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Color from "./Color";
 
-export default function ColorList({ colors = [], onRemoveColor = f => f }) {
-  if (!colors.length) return <div>No Colors Listed.</div>;
+export default function ColorList({
+  colors = [],
+  onRemoveColor = f => f,
+  onRateColor = f => f
+}) {
+  if (!colors.length) return <div>No Colors Listed. (Add a Color)</div>;
   return (
-    <div>
+    <div className="color-list">
       {
-        colors.map(color => <Color key={color.id} {...color} onRemove={onRemoveColor} />)
+        colors.map(color => (
+          <Color
+            key={color.id}
+            {...color}
+            onRemove={onRemoveColor}
+            onRate={onRateColor}
+          />
+        ))
       }
     </div>
   );
