72ac6d57d21217b26f272732546653cd3f3b96b3
K Tact <ktact.dev@gmail.com>
2021-06-19 11:15:37 +0900

    指定個数のスターを作成できるようStarRatingコンポーネントのインタフェースを変更

--- chapter-06/star-rating-5/src/StarRating.js
+++ chapter-06/star-rating-5/src/StarRating.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating() {
-  return [
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="grey" />,
-    <FaStar color="grey" />
-  ];
+const createArray = length => [...Array(length)];
+
+const Star = ({ selected = false }) => (
+  <FaStar color={selected ? "red" : "grey"} />
+);
+
+export default function StarRating({ totalStars = 5 }) {
+  return createArray(totalStars).map((n, i) => <Star key={i} />);
 }
