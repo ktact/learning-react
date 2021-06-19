7a09f842aeadd4d46e76e3059459df9b1746498d
K Tact <ktact.dev@gmail.com>
2021-06-19 16:30:46 +0900

    ローカルストレージの利用

--- chapter-08/src/App.js
+++ chapter-08/src/App.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from "react";
 
+const loadJSON = key => key && JSON.parse(localStorage.getItem(key));
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+
 function GitHubUser({ login }) {
-  const [data, setData] = useState();
+  const [data, setData] = useState(loadJSON(`user:${login}`));
+
+  useEffect(() => {
+    if (!data) return;
+    if (data.login === login) return;
+
+    const { name, avatar_url, location } = data;
+
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location
+    });
+  }, [data]);
 
   useEffect(() => {
     if (!login) return;
+    if (data && data.login === login) return;
 
     fetch(`https://api.github.com/users/${login}`)
       .then(response => response.json())
